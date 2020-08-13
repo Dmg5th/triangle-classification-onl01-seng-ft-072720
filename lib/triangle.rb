72a152149ae10470095e56378acc944e51a1558a
Daniel Gould <dmgould310@gmail.com>
2020-08-13 12:17:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,29 @@
 class Triangle
-  
-  def initialize(side_1, side_2, side_3)
+  def initialize(side_1,side_2,side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end 
-  
-  def kind 
+  end
+
+  def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    if (@side_1 == @side_2) && (@side_2 == @side_3)
-      :equilateral
-    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-      :isosceles
-    elsif !(@side_1 == @side_2) || !(@side_2 == @side_3) || !(@side_1 == @side_3)
-      :scalene
-    else 
-    end 
-  end 
-end 
-  
-  
-  
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
+
 end
 
 class TriangleError < StandardError
-    # triangle error code
-  end
+  # triangle error code
+end
