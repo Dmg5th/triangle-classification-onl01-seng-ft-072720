9b5b21265e607bf0688e128a7cdaa7905846a878
Daniel Gould <dmgould310@gmail.com>
2020-08-13 12:05:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,12 @@
 class Triangle
   
   def initialize(side_1, side_2, side_3)
-    self.side_1 = side_1
-    self.side_2 = side_2
-    self.side_3 = side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end 
   
   def kind 
-    if self
+    if @side_1 == @side_2
   
 end
