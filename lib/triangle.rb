d7ab66e413087742be49c03ff9c85d9ba9bd3482
Daniel Gould <dmgould310@gmail.com>
2020-08-13 12:02:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,11 @@
 class Triangle
   
-  def initialize()
+  def initialize(side_1, side_2, side_3)
+    self.side_1 = side_1
+    self.side_2 = side_2
+    self.side_3 = side_3
+  end 
+  
+  
+  
 end
